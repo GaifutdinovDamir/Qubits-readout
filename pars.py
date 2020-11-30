a3c29db43d958caeae97e66a8821eb00db56bc6a
TerentevVSs <71519431+TerentevVSs@users.noreply.github.com>
2020-11-30 22:56:20 +0300

    Add files via upload

--- pars.py
+++ pars.py
@@ -1,17 +1,25 @@
 import csv
-with open("zeros.txt", "r") as file:
+import random
+
+with open("zeros2.txt", "r") as file:
     a = file.readlines()
     zeros = []
+    label = []
     for elem in a[1:]:
         zeros.append(complex(elem.strip()))
-with open("ones.txt", "r") as file:
+        label.append(0)
+with open("ones2.txt", "r") as file:
     a = file.readlines()
     ones = []
     for elem in a[1:]:
         ones.append(complex(elem.strip()))
+        label.append(1)
+data = zeros + ones
+for j in range(len(data)):
+    r = random.randint(0, len(data))
+    data[j], data[r] = data[r], data[j]
+    label[j], label[r] = label[r], label[j]
 with open("Training examples.csv", "a") as file:
     file_writer = csv.writer(file, delimiter=",", lineterminator="\r")
-    for i in range(len(zeros)):
-        file_writer.writerow([zeros[i].real, zeros[i].imag, 0])
-    for i in range(len(ones)):
-        file_writer.writerow([ones[i].real, ones[i].imag, 1])
+    for i in range(len(data)):
+        file_writer.writerow([data[i].real, data[i].imag, label[i]])
